040f9465f7f253815dcbe63cde6682e246b53110
HydraHigh <hydrahigh22@gmail.com>
2020-03-22 21:52:09 +0100

    mejora de la ventana

--- Codigo/main.py
+++ Codigo/main.py
@@ -4,9 +4,11 @@ import pyglet.clock
 
 pygame.init()
 
-size = 800, 600 
+size = 1280, 720 
+icon = pygame.image.load('..\Arte\Grafico\Icon\icono.png')
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Bolim')
+pygame.display.set_icon(icon)
 
 fps = 60
 Black = (0, 0, 0)
@@ -17,7 +19,13 @@ def main():
 	reloj = pyglet.clock.Clock()
 	reloj.set_fps_limit(fps)
 	quit = False
-
+	full_screen = False
+	viruses = pygame.image.load('..\Arte\Grafico\Icon\icono.png')
+	power = open('..\Data\power.txt', 'r')
+	pygame.mixer.music.load('..\Arte\Audio\Feelings-8-bit.wav')
+	pygame.mixer.music.play(100000)
+	pygame.mixer.music.set_volume(0.5)
+	
 
 	while quit != True:
 
@@ -25,6 +33,19 @@ def main():
 			if evento.type == pygame.QUIT:
 				quit = True
 
+			if evento.type == pygame.KEYDOWN:
+				if evento.key == pygame.K_ESCAPE:
+						
+						full_screen = not full_screen
+
+						if full_screen:
+							pygame.display.set_mode((size), pygame.FULLSCREEN)
+
+						else:
+							pygame.display.set_mode((size), 0)
+
+		screen.fill(White)
+		screen.blit(viruses, (390, 110))
 
 
 

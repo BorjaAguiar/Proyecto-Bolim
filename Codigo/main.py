004998aeff62ccc5cb12fc8d5b87d75b2794f697
HydraHigh <hydrahigh22@gmail.com>
2020-03-24 18:14:18 +0100

    añadido p2(con bugs)

--- Codigo/main.py
+++ Codigo/main.py
@@ -1,6 +1,5 @@
 import sys
 import pygame
-import pyglet.clock
 import player
 
 pygame.init()
@@ -10,22 +9,18 @@ icon = pygame.image.load('..\Arte\Grafico\Icon\icono.png')
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Bolim')
 pygame.display.set_icon(icon)
-
-#fps = 0
 Black = (0, 0, 0)
 White = (255, 255, 255)
 
 
 def main():
-	#reloj = pyglet.clock.Clock()
-	#reloj.set_fps_limit(fps)
 	clock = pygame.time.Clock()
 	quit = False
 	full_screen = False
 	music = True
-	viruses = pygame.image.load('..\Arte\Grafico\Icon\icono.png')
 	power = open('..\Data\power.txt', 'r')
 	P1 = player.P1((625, 315))
+	P2 = player.P2((625, 255))
 	pygame.mixer.music.load('..\Arte\Audio\Feelings-8-bit.wav')
 	pygame.mixer.music.play(100000)
 	pygame.mixer.music.set_volume(0.5)
@@ -34,21 +29,21 @@ def main():
 	while quit != True:
 
 		#Funciones de la ventana
-		for evento in pygame.event.get():
-			if evento.type == pygame.QUIT:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
 				quit = True
 
-			if evento.type == pygame.KEYDOWN:
-				if evento.key == pygame.K_ESCAPE:	
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:	
 					full_screen = not full_screen
 
 					if full_screen:
 						pygame.display.set_mode((size), pygame.FULLSCREEN)
-
 					else:
 						pygame.display.set_mode((size), 0)
 
-				if evento.key == pygame.K_p:
+				if event.key == pygame.K_p:
+
 					if music == True:
 						pygame.mixer.music.pause()
 						music = False
@@ -56,26 +51,25 @@ def main():
 						pygame.mixer.music.unpause()
 						music = True
 
-				if evento.key == pygame.K_KP_PLUS:
+				if event.key == pygame.K_UP:
 					volume = pygame.mixer.music.get_volume()
 					pygame.mixer.music.set_volume(volume + 0.1)
 					#print(volume)
 
-				if evento.key == pygame.K_KP_MINUS:
+				if event.key == pygame.K_DOWN:
 					volume = pygame.mixer.music.get_volume()
 					pygame.mixer.music.set_volume(volume - 0.1)
 					#print(volume)
 
-		P1.handle_event(evento)
+	
+
+
 		screen.fill(White)
+		P1.handle_event(event)
+		P2.handle_event(event)
 		screen.blit(P1.image, P1.rect)
-		#screen.blit(viruses, (390, 110))
-
-
-
-
+		screen.blit(P2.image, P2.rect)
 		pygame.display.update()
 		clock.tick(20)
-		#reloj.set_fps_limit(fps)
 main()
 pygame.quit()
--- Codigo/player.py
+++ Codigo/player.py
@@ -53,7 +53,7 @@ class P1(pygame.sprite.Sprite):
 
     def handle_event(self, event):
         if event.type == pygame.QUIT:
-            game_over = True
+            quit = True
 
         if event.type == pygame.KEYDOWN:
 
@@ -76,3 +76,45 @@ class P1(pygame.sprite.Sprite):
                 self.update('stand_up')
             if event.key == pygame.K_s:
                 self.update('stand_down')
+
+class P2(P1):
+    def __init__(self, position):
+
+        self.sheet = pygame.image.load('..\Arte\Grafico\Sprites\p2walk.png')
+        self.sheet.set_clip(pygame.Rect(7, 123, 30, 57))
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+        self.rect = self.image.get_rect()
+        self.rect.topleft = position
+        self.frame = 0
+        self.left_states = { 0: (8, 64, 36, 51), 1: (72, 64, 36, 51), 2: (136, 64, 36, 51), 3: (200, 64, 36, 51), 4: (264, 64, 36, 51), 5: (328, 64, 36, 51), 6: (392, 64, 36, 51), 7: (456, 64, 36, 51), 8: (520, 64, 36, 51) }
+        self.right_states = { 0: (0, 192, 36, 51), 1: (64, 192, 36, 51), 2: (128, 192, 36, 51), 3: (192, 192, 36, 51), 4: (256, 192, 36, 51), 5: (320, 192, 36, 51), 6: (384, 192, 36, 51), 7: (448, 192, 36, 51), 8: (512, 192, 36, 51) }
+        self.up_states = { 0: (0, 0, 30, 48), 1: (64, 0, 30, 48), 2: (135, 0, 30, 48), 3: (199, 0, 30, 48), 4: (263, 0, 30, 48), 5: (327, 0, 30, 48), 6: (391, 0, 30, 48), 7: (456, 0, 30, 48), 8: (519, 0, 30, 48) }
+        self.down_states = { 0: (7, 123, 30, 57), 1: (71, 123, 30, 57), 2: (135, 123, 30, 57), 3: (200, 123, 30, 57), 4: (263, 123, 30, 57), 5: (327, 123, 30, 57), 6: (391, 123, 30, 57), 7: (455, 123, 30, 57), 8: (519, 123, 30, 57) }
+
+        def update(self, direction):
+
+            if direction == 'left':
+                self.clip(self.left_states)
+                self.rect.x = self.rect.x + 100
+                self.rect.y = 0
+            if direction == 'right':
+                self.clip(self.right_states)
+                self.rect.x = self.rect.x - 100
+                self.rect.y = 0
+            if direction == 'up':
+                self.clip(self.up_states)
+                self.rect.x = 0
+                self.rect.y = self.rect.y + 100
+            if direction == 'down':
+                self.clip(self.down_states)
+                self.rect.x = 0
+                self.rect.y = self.rect.y - 100
+
+            if direction == 'stand_left':
+                self.clip(self.left_states[0])
+            if direction == 'stand_right':
+                self.clip(self.right_states[0])
+            if direction == 'stand_up':
+                self.clip(self.up_states[0])
+            if direction == 'stand_down':
+                self.clip(self.down_states[0])
